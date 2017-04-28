c808bf75ea4afa0932eab5a6d52c5dfac83e8ade
Parker King <parker.king0807@gmail.com>
2017-04-28 09:34:13 -0600

    Adding service changes

--- public/components/services/companyService.js
+++ public/components/services/companyService.js
@@ -3,28 +3,28 @@ var app = angular.module("CompanyApp");
 app.service("companyService", ["$http", function ($http) {
 
     this.getId = function(){
-        return $http.get("http://localhost:3000/api/company/getId").then(function(response){
+        return $http.get("api/company/getId").then(function(response){
             return response.data
         })
     };
 
     this.getAll = function(){
-        return $http.get("http://localhost:3000/api/company/all").then(function(response){
+        return $http.get("api/company/all").then(function(response){
             return response.data;
         })
     };
     this.edit = function(id, add){
-        return $http.put("http://localhost:3000/api/company/" + id, add).then(function(response){
+        return $http.put("api/company/" + id, add).then(function(response){
             return response.data
         })
     };
 this.getUsers = function(){
-        return $http.get("http://localhost:3000/api/company/").then(function(response){
+        return $http.get("api/company/").then(function(response){
             return response.data
         })
 };
 this.newCompany = function(comp){
-    return $http.post("http://localhost:3000/api/company/", comp).then(function(response){
+    return $http.post("api/company/", comp).then(function(response){
         return response.data;
     })
 }
